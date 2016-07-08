06f9fedc53e412992d836869e849398f30779f38
Wilhelmina Drengwitz <ghthor@gmail.com>
2016-07-08 18:26:23 -0400

    [stockprice] add backtracking implementation for stock price search

--- stockprice/main.go
+++ stockprice/main.go
@@ -11,8 +11,38 @@ import (
 type Prices []int
 
 func (p Prices) MaxProfit() (profit int) {
-	// TODO: Implementation
-	return 0
+	if len(p) < 2 {
+		panic("len(prices array) < 2")
+	}
+
+	// Set initial price seen
+	highestPriceSeen := p[len(p)-1]
+	highestProfit := p[len(p)-1] - p[len(p)-2]
+
+	for i := len(p) - 2; i >= 0; i-- {
+		if p[i] > highestPriceSeen {
+			remaining := p[:i+1]
+			if len(remaining) < 2 {
+				continue
+			}
+
+			profit := remaining.MaxProfit()
+			if profit > highestProfit {
+				return profit
+			} else {
+				highestPriceSeen = p[i]
+				continue
+			}
+
+		} else {
+			profit := highestPriceSeen - p[i]
+			if profit > highestProfit {
+				highestProfit = profit
+			}
+		}
+	}
+
+	return highestProfit
 }
 
 func NewPricesFromStrings(pricesStr []string) (Prices, error) {
